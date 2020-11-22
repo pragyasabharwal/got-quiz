3cb6e738885f67e534f79f345e2d6a6ef4b96e85
Pragya <sabharwalpragya@gmail.com>
2020-11-22 17:31:20 +0530

    feat: high score

--- got.js
+++ got.js
@@ -11,47 +11,62 @@ console.log("\n")
 {
     if (response.toUpperCase() === "YES") {
         var userName = question.log(chalk.magenta("What is your name?"))
-        console.log("\n") 
-        log(chalk.bgBlue("Hi " + userName + ". Let the game begin!")) 
-        console.log("\n") 
+        console.log("\n")
+        log(chalk.bgBlue("Hi " + userName + ". Let the game begin!"))
+        console.log("\n")
         log(chalk.black.bgYellowBright("There will be three options for every question. Choose any three of the options by writing a b or c.\n"))
 
-        score=0;
+        score = 0;
 
         questions = [{
-            question:"Where did Daenerys Targaryen eventually hatch her dragon eggs?\na- fireplace\nb- frozen cave\nc- funeral pyre\n",
-            answer: "c"},
+                question: "Where did Daenerys Targaryen eventually hatch her dragon eggs?\na- fireplace\nb- frozen cave\nc- funeral pyre\n",
+                answer: "c"
+            },
             {
-            question:"The phrase 'Valar Morghulis' or 'all men must die' is usually responded with?\na- Valar Rohnas\nb- Valar Dohaeris\nc- Valar GoGo\n",
-            answer: "b"},
+                question: "The phrase 'Valar Morghulis' or 'all men must die' is usually responded with?\na- Valar Rohnas\nb- Valar Dohaeris\nc- Valar GoGo\n",
+                answer: "b"
+            },
             {
-            question:"American actor Peter Dinklage, who plays Tyrion Lannister, also had a starring role in this fantasy franchise.\na- Lord of the Rings\nb- The Chronicles of Narnia\nc- Highlander\n",
-            answer: "b"
-          },{
-            question:"What is the only thing that can put out volatile Wildfire?\na- Sand\nb- Water\nc- Sunlight\n",
-             answer: "a"
-          }]
+                question: "American actor Peter Dinklage, who plays Tyrion Lannister, also had a starring role in this fantasy franchise.\na- Lord of the Rings\nb- The Chronicles of Narnia\nc- Highlander\n",
+                answer: "b"
+            }, {
+                question: "What is the only thing that can put out volatile Wildfire?\na- Sand\nb- Water\nc- Sunlight\n",
+                answer: "a"
+            }
+        ]
 
-          function play(question, answer)
-          
-          {
-             var userAns = readlineSync.question(question)
-             if(userAns.toUpperCase()===answer)
-             {
-                 score++
-                 log(chalk.green("You know this answer! " + "\n" + "Points: " + score))
-                 console.log("--------")
-             }
-             else{
+        function play(question, answer)
+
+        {
+            var userAns = readlineSync.question(question)
+            if (userAns.toUpperCase() === answer) {
+                score++
+                log(chalk.green("You know this answer! " + "\n" + "Points: " + score))
+                console.log("--------")
+            } else {
                 score--
-                log(chalk.red("You know nothing, " + userName +" Snow :( " + "\n" + "Points: " + score))
-                console.log("--------") 
-             }
+                log(chalk.red("You know nothing, " + userName + " Snow :( " + "\n" + "Points: " + score))
+                console.log("--------")
+            }
 
-             for(var i=0;i<questions.lenght;i++){
-                 play(questions[i].question, questions[i].answer)
-             }
-          }
+            for (var i = 0; i < questions.lenght; i++) {
+                play(questions[i].question, questions[i].answer)
+            }
 
-        
-    }
+        }
+        log(chalk.bgMagenta("Thanks for playing. Your total score is " + score + "\n"))
+
+        log(chalk.yellowBright("\nPrevious Top Scorers:\nPragya: 3\nKriti: 2\nRahul: 2\n"))
+
+
+        highScores = [3, 2, 1]
+
+        for (i = 0; i < highScores.length; i++) {
+            if (score >= highScores[i]) {
+                log(chalk.bold.black.bgGreen("You are a top scorer. Please send a screenshot of your score!"))
+                break
+            }
+        }
+
+    } else
+        console.log("Thanks for coming.. Goodbye!")
